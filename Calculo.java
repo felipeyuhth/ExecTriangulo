af492b499e837fe9b6b44854f0751123018ac9bf
FelipeQueiroz <felipeyuhth@users.noreply.github.com>
2020-03-17 19:19:31 -0300

    Add files via upload

---
+++ Calculo.java
@@ -0,0 +1,21 @@
+package Triangulo;
+import java.util.Scanner;
+public class Calculo {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		double base, altura, area;
+		
+		Triangulo Calculo = new Triangulo();
+		
+		System.out.println("Digite o valor da base: ");
+		base=in.nextDouble();
+		
+		System.out.println("Digite o valor da altura: ");
+		altura=in.nextDouble();
+		
+		Calculo.base = base;
+		Calculo.altura = altura;
+		Calculo.area();
+		System.out.println("O resultado é "+Calculo.area);
+	}
+}
---
+++ Triangulo.java
@@ -0,0 +1,11 @@
+package Triangulo;
+
+public class Triangulo {
+	double base;
+	double altura;
+	double area;
+	void area(){
+		area = base * altura / 2;
+	}
+}
+	
